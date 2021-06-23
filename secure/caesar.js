bbb36750c7ffe4bf77be3b7b2fbc425af309a269
hyebbi <hyebbi99@naver.com>
2021-06-23 12:43:57 +0900

    Add security

---
+++ secure/caesar.js
@@ -0,0 +1,39 @@
+alpha = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "s",
+  "y",
+  "z",
+];
+
+str ="come to rome"
+key = 3;
+
+// alpha.forEach(function (item, index, arr2) {
+//   console.log(item, index, arr2);
+// });
+
+for(var i in str){
+    idx =(parseInt(getAlpha(str[i])+key)alphabet.length);
+};
